dbd3d3fd1edafe8e01f228d736756331bf4324bc
Wizan05 <107671723+Wizan05@users.noreply.github.com>
2022-06-17 23:23:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Wizan05/aftb
+  git clone https://github.com/Wizan05/aftb.git /aftb
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /aftb
